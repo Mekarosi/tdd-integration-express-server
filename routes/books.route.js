08c726c0862c6a4997418fa4d5dfdc12aa5ad850
Mekarosi <mekarosi_assa@yahoo.com>
2024-01-31 10:32:01 -0800

    Wrote integration test for failure on invalid post body

--- routes/books.route.js
+++ routes/books.route.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const router = express.Router()
+const booksData = require('../data/books.json')
+const {check, validationResult} = require('express-validator')
 
 router.get('/', (req, res) => {
-    res.send({
-        message: 'server is ready'
-    })
-
+   res.json(booksData)
 })
+
+router.post('/', [
+    check('name', 'Book name is required').not().isEmpty(),
+    check('author', 'Author name is required').not().isEmpty(),  
+] ,
+(req, res) => {
+    const errors = validationResult(req)
+    
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            errors: errors.array()
+        })
+    }
+})
+
 module.exports = router
--- tests/bookRoute.integration.test.js
+++ tests/bookRoute.integration.test.js
@@ -10,7 +10,7 @@ app.use('/api/books', booksRoute)
 describe("Integration test for the books API", () => {
     it("GET /api/books - success - get all the books", async () => {
        const {body, statusCode} = await request(app).get('/api/books')
-
+ 
        expect(body).toEqual(
         expect.arrayContaining([
             expect.objectContaining({
@@ -21,5 +21,26 @@ describe("Integration test for the books API", () => {
         ])
        )
        expect(statusCode).toBe(200)
+    })
+
+    it("POST /api/books - failure on invalid post body", async () => {
+          
+        const { body, statusCode } = await request(app).post("/api/books").send({
+            name: "",
+            author: "John Travolta"
+        })
+
+        expect(statusCode).toBe(400)
+        expect(body).toEqual({
+            errors: [
+                {
+                    "location": "body",
+                    "msg": "Book name is required",
+                    "path": "name",
+                    "type": "field",
+                    "value": "",
+                }
+            ]
+        })
     })
 })
