68197659fafd2a998530f3ad8d8ce65cca2e4315
Mekarosi <mekarosi_assa@yahoo.com>
2024-01-31 08:03:26 -0800

    Wrote an integration test to get success message to GET all books

--- tests/bookRoute.integration.test.js
+++ tests/bookRoute.integration.test.js
@@ -3,6 +3,23 @@ const request = require('supertest')
 const booksRoute = require('../routes/books.route')
 
 const app = express()
-
+ 
 app.use(express.json())
 app.use('/api/books', booksRoute)
+
+describe("Integration test for the books API", () => {
+    it("GET /api/books - success - get all the books", async () => {
+       const {body, statusCode} = await request(app).get('/api/books')
+
+       expect(body).toEqual(
+        expect.arrayContaining([
+            expect.objectContaining({
+                id: expect.any(Number),
+                name: expect.any(String),
+                author: expect.any(String)
+            })
+        ])
+       )
+       expect(statusCode).toBe(200)
+    })
+})
